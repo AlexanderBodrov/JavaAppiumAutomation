821bbf302270165d11491f24d8e1196f28e59bf3
bodrov <a.bodrov@korona.net>
2025-12-02 18:05:46 +0300

    Part 3 Test 1

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -1,43 +1,49 @@
 import io.appium.java_client.android.AndroidDriver;
+import java.net.MalformedURLException;
+import java.net.URL;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import java.net.MalformedURLException;
-import java.net.URL;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class FirstTest {
-
     private AndroidDriver driver;
 
     @Before
     public void setUp() throws MalformedURLException {
         DesiredCapabilities capabilities = new DesiredCapabilities();
-
         capabilities.setCapability("platformName", "Android");
-
         capabilities.setCapability("newCommandTimeout", "3600");
         capabilities.setCapability("deviceName", "Main");
-        capabilities.setCapability("platformVersion", "14");
+        capabilities.setCapability("platformVersion", "10");
         capabilities.setCapability("automationName", "UiAutomator2");
         capabilities.setCapability("appPackage", "org.wikipedia");
         capabilities.setCapability("appActivity", ".main.MainActivity");
         capabilities.setCapability("app", "/Users/aleksandr/Desktop/JavaAppiumAutomation/JavaAppiumAutomation/apks/org.wikipedia.apk");
-
         driver = new AndroidDriver(new URL("http://0.0.0.0:4723/wd/hub"), capabilities);
     }
 
     @After
-    public void tearDown()
-    {
-        if (driver != null) {
-            driver.quit();
-        }
+    public void tearDown() {
+       driver.quit();
     }
 
     @Test
-    public void checkFirstTest()
-    {
-        System.out.println("First test run");
+    public void firstTest() {
+        assertElementHasText(
+                By.xpath("//*[contains(@text,'Search Wikipedia')]"),
+                "Search Wikipedia",
+                "expected text not founded"
+        );
+    }
+
+    private void assertElementHasText(By by, String expectedText, String errorMessage) {
+        WebElement element = driver.findElement(by);
+        String actualText = element.getText();
+        assertTrue(actualText.contains(expectedText), errorMessage);
     }
 }
